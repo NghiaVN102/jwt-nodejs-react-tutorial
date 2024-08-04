a7093667f85489312b8f8a2dc6123ad208b359d8
NGHIAVN102 <nghiabn102@gmail.com>
2024-08-04 22:21:17 +0900

    generate model/migration/seeder

---
+++ src/seeders/seeders/20240804131031-demo-user.js
@@ -0,0 +1,40 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * 
+    */
+    await queryInterface.bulkInsert('Users', 
+    [
+      {
+      email: 'John Doe',
+      password: '123',
+      username: 'fake11'
+      },
+      {
+        email: 'John Doe2',
+        password: '123',
+        username: 'fake12'
+      },
+      {
+        email: 'John Doe3',
+        password: '123',
+        username: 'fake13'
+      }
+    ], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
